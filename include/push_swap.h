405e59b4700ba8d6f1c04d7def5e0e8aedeadfc9
Ton Nom <theosykas@icloud.com>
2025-05-30 19:03:16 +0200

    struct_prime

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,22 +6,25 @@
 /*   By: theo <theo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 18:29:55 by theo              #+#    #+#             */
-/*   Updated: 2025/05/30 18:53:33 by theo             ###   ########.fr       */
+/*   Updated: 2025/05/30 19:03:02 by theo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef	struct s_swap
+typedef struct	s_swap
 {
 	int	swap;
 }	t_swap;
 
+/*utils.c*/
+
 /*swap.c*/
 int	main(int ac, char **av);
 
-# endif
+#endif
 
 /*atoi maybe good*/
 /*pile a et pile b*/
+/*t_swap *swap*/
